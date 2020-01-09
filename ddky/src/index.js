bf18e75f5bfdfafed380de6fa7c4eee7aac74b05
luodecheng1911 <55774646+luodecheng1911@users.noreply.github.com>
2020-01-09 15:14:00 +0800

    第七次提交

--- ddky/src/index.js
+++ ddky/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter,BrowserRouter } from 'react-router-dom';
+import store from './store';
 
 const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 ReactDOM.render(
-
-    <Router>
-        <App />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
